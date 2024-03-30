b68bbd9eb57b350ca72e91bec803fd6811a51cc4
yulianaj <yuliana.pa2016@gmail.com>
2024-03-30 10:16:48 -0400

    add roman numeral files

--- 101-kittens-carousel-static-website-ec2/userdata.sh
+++ 101-kittens-carousel-static-website-ec2/userdata.sh
@@ -1,8 +1,22 @@
 #!bin/bash
 
+#update the OS
 yum update -y
 
+#install apache
 yum install httpd -y
 
-FOLDER=
-wget
+#copy content to /var/www/html
+cd /var/www/html
+
+FOLDER="https://raw.githubusercontent.com/jones-yuliana/my-repository/main/101-kittens-carousel-static-website-ec2/static-web"
+
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+
+#start and enable Apache service
+systemctl start httpd
+systemctl enable httpd
+systemctl status httpd
